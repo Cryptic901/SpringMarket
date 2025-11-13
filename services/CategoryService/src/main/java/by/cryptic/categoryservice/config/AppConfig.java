453926186deb4b9d5cf71101d9db1e14e31be138
Cryptic901 <luckyartem901@gmail.com>
2025-11-13 21:32:13 +0300

    Update app images to k8s

--- services/CategoryService/src/main/java/by/cryptic/categoryservice/config/AppConfig.java
+++ services/CategoryService/src/main/java/by/cryptic/categoryservice/config/AppConfig.java
@@ -4,11 +4,8 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import io.opentelemetry.sdk.trace.SdkTracerProvider;
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
