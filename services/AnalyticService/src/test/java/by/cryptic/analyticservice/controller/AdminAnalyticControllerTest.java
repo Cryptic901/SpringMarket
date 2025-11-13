38e692a2ac4ba2beb08d210696c159d30e0a89c1
Cryptic901 <luckyartem901@gmail.com>
2025-11-13 21:48:31 +0300

    Update app images to k8s

--- services/AnalyticService/src/test/java/by/cryptic/analyticservice/controller/AdminAnalyticControllerTest.java
+++ services/AnalyticService/src/test/java/by/cryptic/analyticservice/controller/AdminAnalyticControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
